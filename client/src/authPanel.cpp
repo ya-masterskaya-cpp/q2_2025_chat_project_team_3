969a919f071d014131da297b04a50ed977292b4b
cuteBoiButt <cuteBoiButt@protonmail.com>
2025-06-17 18:40:39 +0000

    back button

--- client/src/authPanel.cpp
+++ client/src/authPanel.cpp
@@ -4,49 +4,55 @@
 #include <client/passwordUtil.h>
 #include <optional>
 
-enum { ID_LOGIN = wxID_HIGHEST+10, ID_REGISTER };
-
-wxBEGIN_EVENT_TABLE(AuthPanel, wxPanel)
-    EVT_BUTTON(ID_LOGIN, AuthPanel::OnLogin)
-    EVT_BUTTON(ID_REGISTER, AuthPanel::OnRegister)
-wxEND_EVENT_TABLE()
-
 void AuthPanel::SetButtonsEnabled(bool enabled) {
-    usernameInput->Enable(enabled);
-    passwordInput->Enable(enabled);
-    loginButton->Enable(enabled);
-    registerButton->Enable(enabled);
+    m_usernameInput->Enable(enabled);
+    m_passwordInput->Enable(enabled);
+    m_loginButton->Enable(enabled);
+    m_registerButton->Enable(enabled);
 }
 
 AuthPanel::AuthPanel(MainWidget* parent) : wxPanel(parent), mainWin(parent) {
     auto* sizer = new wxBoxSizer(wxVERTICAL);
     sizer->AddStretchSpacer();
     sizer->Add(new wxStaticText(this, wxID_ANY, "Username:"), 0, wxALL | wxALIGN_CENTER, 5);
-    usernameInput = new wxTextCtrl(this, wxID_ANY);
-    sizer->Add(usernameInput, 0, wxALL | wxEXPAND, 5);
+
+    m_usernameInput = new wxTextCtrl(this, wxID_ANY);
+    sizer->Add(m_usernameInput, 0, wxALL | wxEXPAND, 5);
     sizer->Add(new wxStaticText(this, wxID_ANY, "Password:"), 0, wxALL | wxALIGN_CENTER, 5);
-    passwordInput = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD);
-    sizer->Add(passwordInput, 0, wxALL | wxEXPAND, 5);
+    m_passwordInput = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD);
+    sizer->Add(m_passwordInput, 0, wxALL | wxEXPAND, 5);
+
     auto* btnSizer = new wxBoxSizer(wxHORIZONTAL);
-    loginButton = new wxButton(this, ID_LOGIN, "Login");
-    registerButton = new wxButton(this, ID_REGISTER, "Register");
-    btnSizer->Add(loginButton, 1, wxALL, 5);
-    btnSizer->Add(registerButton, 1, wxALL, 5);
+    m_loginButton = new wxButton(this, wxID_ANY, "Login");
+    m_registerButton = new wxButton(this, wxID_ANY, "Register");
+    m_backButton = new wxButton(this, wxID_ANY, "Back");
+    btnSizer->Add(m_loginButton, 1, wxALL, 5);
+    btnSizer->Add(m_registerButton, 1, wxALL, 5);
+    btnSizer->Add(m_backButton, 1, wxALL, 5);
+
     sizer->Add(btnSizer, 0, wxALIGN_CENTER);
     sizer->AddStretchSpacer();
     SetSizer(sizer);
+
+    m_loginButton   ->Bind(wxEVT_BUTTON, &AuthPanel::OnLogin,    this);
+    m_registerButton->Bind(wxEVT_BUTTON, &AuthPanel::OnRegister, this);
+    m_backButton    ->Bind(wxEVT_BUTTON, &AuthPanel::OnBack,     this);
 
     //SetButtonsEnabled(false); // Disable by default until connection is ready
 }
 
 void AuthPanel::OnLogin(wxCommandEvent&) {
-    m_password = passwordInput->GetValue();
-    mainWin->wsClient->requestInitialAuth(usernameInput->GetValue().ToStdString());
+    m_password = m_passwordInput->GetValue();
+    mainWin->wsClient->requestInitialAuth(m_usernameInput->GetValue().ToStdString());
 }
 
 void AuthPanel::OnRegister(wxCommandEvent&) {
-    m_password = passwordInput->GetValue();
-    mainWin->wsClient->requestInitialRegister(usernameInput->GetValue().ToStdString());
+    m_password = m_passwordInput->GetValue();
+    mainWin->wsClient->requestInitialRegister(m_usernameInput->GetValue().ToStdString());
+}
+
+void AuthPanel::OnBack(wxCommandEvent&) {
+    mainWin->ShowInitial();
 }
 
 void AuthPanel::HandleRegisterContinue() {
