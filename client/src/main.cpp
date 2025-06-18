d1a50a82136dc9d49a14c5cba84af45d7fa161ca
Aleksey Dronov <cuteboibutt@protonmail.com>
2025-06-18 21:39:19 +0300

    Rebranding (#64)
    Resolves #13

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -8,9 +8,13 @@ class MyApp : public wxApp {
 public:
     MainWidget* mw = nullptr;
     virtual bool OnInit() override {
+        this->SetVendorName("0xCAFEBABE");
+        this->SetAppName("SlightlyPrettyChat");
+        this->SetAppDisplayName("Slightly Pretty Chat™");
+
         // Start Drogon (networking) in a background thread
         drogon::app().setLogLevel(trantor::Logger::kTrace);
-        
+
         drogonThread = std::thread([] {
             drogon::app().run();
         });
