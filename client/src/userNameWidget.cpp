e5a2ee4b73c76af33f655aaefc20975636e288a2
Nikita <ipad.air.nokia@yandex.ru>
2025-06-20 14:57:50 +0300

    remake commit

--- client/src/userNameWidget.cpp
+++ client/src/userNameWidget.cpp
@@ -27,8 +27,8 @@ UserNameWidget::UserNameWidget(wxWindow* parent, const User& user)
     sizer->Add(m_usernameText, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
     SetSizer(sizer);
 
-    Bind(wxEVT_RIGHT_DOWN, &UserNameWidget::OnRightClick, this);
-    m_usernameText->Bind(wxEVT_RIGHT_DOWN, &UserNameWidget::OnRightClick, this);
+    // Bind(wxEVT_RIGHT_DOWN, &UserNameWidget::OnRightClick, this);
+    // m_usernameText->Bind(wxEVT_RIGHT_DOWN, &UserNameWidget::OnRightClick, this);
 }
 
 void UserNameWidget::OnRightClick(wxMouseEvent& event) {
@@ -41,16 +41,16 @@ void UserNameWidget::OnRightClick(wxMouseEvent& event) {
     // Store the selected user for menu actions
     //m_selectedUser = clickedWidget->GetUser();
     
-    // wxMenu menu;
-    // menu.Append(wxID_ANY, "Private message");
-    // menu.AppendSeparator();
-    // menu.Append(wxID_ANY, "Kick user");
-    // menu.Append(wxID_ANY, "Ban user");
+    wxMenu menu;
+    menu.Append(wxID_ANY, "Private message");
+    menu.AppendSeparator();
+    menu.Append(wxID_ANY, "Kick user");
+    menu.Append(wxID_ANY, "Ban user");
     
     // Bind menu events
     //menu.Bind(wxEVT_MENU, &UserListPanel::OnPrivateMessage, this, ID_PRIVATE_MESSAGE);
     //menu.Bind(wxEVT_MENU, &UserListPanel::OnKickUser, this, ID_KICK_USER);
     //menu.Bind(wxEVT_MENU, &UserListPanel::OnBanUser, this, ID_BAN_USER);
     
-    // PopupMenu(&menu, event.GetPosition());
+    PopupMenu(&menu, event.GetPosition());
 }
